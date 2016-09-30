1d88f633731c316168791638bf04d03642155ec4
Grzegorz Lipecki <glipecki@consdata.pl>
2016-09-30 07:33:49 +0200

    tests fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('App: DemoNg2', () => {
   it(`should have as title 'app works!'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toContain('app works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
